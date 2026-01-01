a1bc27002ce9a42d78d489c7cb2fb9af4eceacef
BO HTWE <bohtwe438@gmail.com>
2026-01-01 11:27:18 +0630

    Update BMT_AI.py

--- BMT_AI.py
+++ BMT_AI.py
@@ -127,14 +127,14 @@ def ai_studio_module():
         
         col_main, col_side = st.columns([3, 1])
 
-with col_side:
+        with col_side:
         st.markdown(f"<h3 style='color:{curr['c']}'> SETTINGS</h3>", unsafe_allow_html=True)
         duration = st.selectbox(" DURATION", curr['d_list'])
         resolution = st.selectbox(" RESOLUTION", curr['res'])
         aspect_ratio = st.radio(" RATIO", ["16:9", "9:16", "1:1"])
 
-with col_main:
-        if 'generating' not in st.session_state: st.session_state.generating = False
+        with col_main:
+            if 'generating' not in st.session_state: st.session_state.generating = False
         with col_main:
             if 'generating' not in st.session_state: st.session_state.generating = False
             
